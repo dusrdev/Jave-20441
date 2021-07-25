cdcc0c0e03ab095806bc6f8ccd7786870921c176
David Shnayder <dusrdev@gmail.com>
2021-07-25 14:03:44 +0300

    Updated template

--- Recursion/Template.java
+++ Recursion/Template.java
@@ -3,12 +3,14 @@ package Recursion;
 
 public class Template {
 	public static void main (String[] args) {
+		// Color code the outputs
 		System.out.println("\u001B[40m" + "\033[1;35m");
 		// ###########################################
 
 		
 
 		// ############################################
+		// Reset colors
 		System.out.print("\u001B[37m");
 	}
 }
